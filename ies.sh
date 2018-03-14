a45561146346540c2d960aee86f83ca0167074f1
Amriez <36630911+Amriez@users.noreply.github.com>
2018-03-14 12:19:15 +0000

    Ies
    Spam indonesian error systen
    Berbagai spam sms

---
+++ ies.sh
@@ -0,0 +1,85 @@
+clear
+toilet -f bigmono9 -F gay IES SPAM
+sleep 1
+echo "\033[39;1m #INDONESIA ERROR SYSTEM"
+echo "\033[36;1m author AMRiezz"
+sleep 1
+     echo " SPAM SMS "
+     echo "   '(01)'JDID "
+     echo "   '(02)'TELKOMSEL "
+     echo "   '(03)'PHD.      "
+     echo "   '(04)'MATHARIMALL"
+     echo "   '(05)'GRAB "
+     echo "   '(06)'TOKOPEDIA"
+sleep 1
+echo 
+     echo "    (07)INFO "
+     echo "    (08)Keluar"
+echo " pilih nomer nya"
+read ezz
+if [ $ezz = 1 ] || [ $ezz = 01 ]
+then
+clear
+echo "\033[31;1m"
+toilet -f mono12 -F gay "JDID"
+php jdid.php
+fi
+
+if [ $ezz = 02 ] || [ $ezz = 2 ]
+then
+clear
+echo "\033[36;1m"
+toilet -f  mono12 -F gay "TELKOMSEL"
+php telkomsel.php
+fi
+
+if [ $ezz = 03 ] || [ $ezz = 3 ]
+then
+clear 
+echo  "\033[39;1m"
+toilet -f mono12 -F metal "PHD"
+php phd.php
+fi
+
+if [ $ezz = 04 ] || [ $ezz = 4 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "MATAHARIMALL"
+php mataharimall.php
+fi
+
+if [$ezz = 05 ] || [ $ezz = 5 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "GRAB"
+python2 spammer.py
+fi
+
+if [ $ezz = 06 ] || [ $ezz = 6 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "TOKPED"
+php tokped.php
+fi
+
+if [ $ezz = 07 ] || [ $ezz = 7 ]
+then 
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "INFO"
+echo "Dibuat oleh "
+echo "AMRiezz "
+echo "versi 0.1"
+fi
+
+if [ $ezz = 08 ] || [ $ezz =8 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "KELUAR"
+echo "semoga hari mu menyenagkan"
+fi
+
